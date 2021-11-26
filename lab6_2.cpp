0fc22d83af67e531c6a5aeae460382d256509b15
Hutao Doctrine <guy_zxc@hotmail.com>
2021-11-26 17:53:24 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
-
+double deg2rad(double x)
+{
+    return (x*(M_PI/180));
+}
+double rad2deg(double y)
+{
+    return (y*(180/M_PI));
+}
+double findXComponent(double l1,double l2,double a1,double a2)
+{
+    return ((l1*(cos(a1)))+(l2*(cos(a2))));
+}
+double findYComponent(double l1,double l2,double a1,double a2)
+{
+    return ((l1*(sin(a1)))+(l2*(sin(a2))));
+}
+double pythagoras(double xcomp,double ycomp)
+{
+    return sqrt((pow(xcomp,2)+pow(ycomp,2)));
+}
+double showResult(double result_vec_length,double result_vec_direction)
+{
+    return 0;
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
@@ -21,4 +44,6 @@ int main(){
     result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
 
     showResult(result_vec_length,result_vec_direction);
+    cout<<pythagoras(xcomp,ycomp);
+    cout<<"\n"<<rad2deg(atan2(ycomp,xcomp));
 }
