478871bae6bd36f5a18170c9827626534969bfb0
Hutao Doctrine <guy_zxc@hotmail.com>
2021-11-26 18:29:25 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x;
+    int even=0,odd=0;
+    while(x != 0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        if(x !=0){
+            if((x%2) == 0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+    }
+    cout << "Even numbers = " << even;
+    cout << "\n";
+    cout << "Odd numbers = " << odd;
     return 0;
 }
